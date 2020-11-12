5a1332fc62eae8bc508e0f590a9703b5fcd74908
Zeno <zenocbda@gmail.com>
2020-11-12 17:07:10 +0800

    Update dpLearn.go

--- whatever/dpLearn.go
+++ whatever/dpLearn.go
@@ -16,5 +16,26 @@ func FrogJumpDP(n int) int {
 	return solution[n]
 }
 
+func RobotMoveDP(m, n int) int {
+	grid := make([][]int, m+1)
+	for i := 0; i < m+1; i++ {
+		grid[i] = make([]int, n+1)
+	}
+	for i := 0; i < m+1; i ++ {
+		grid[i][1] = 1;
+	}
+	for j := 0; j < n+1; j ++ {
+		grid[1][j] = 1;
+	}
 
+	for i := 2; i < m+1; i ++ {
+		for j := 2; j < n+1; j ++ {
+			grid[i][j] = grid[i-1][j] + grid[i][j-1]
+		}
+	}
 
+	//for _, item := range grid {
+	//	fmt.Printf("%v \n", item)
+	//}
+	return grid[m][n]
+}
