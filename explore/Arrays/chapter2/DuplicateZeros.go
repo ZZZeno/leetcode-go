922cd2164b9cd43dad28a6e462a2671c8a0cc4a7
Zeno <zenocbda@gmail.com>
2020-11-25 01:20:28 +0800

    Create DuplicateZeros.go

---
+++ explore/Arrays/chapter2/DuplicateZeros.go
@@ -0,0 +1,32 @@
+package chapter2
+
+//	Given a fixed length array arr of integers, duplicate each occurrence of zero, shifting the remaining elements to the right.
+//	Note that elements beyond the length of the original array are not written.
+//	Do the above modifications to the input array in place, do not return anything from your function.
+//	
+//	Example 1:
+//	
+//	Input: [1,0,2,3,0,4,5,0]
+//	Output: null
+//	Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
+//	Example 2:
+//	
+//	Input: [1,2,3]
+//	Output: null
+//	Explanation: After calling your function, the input array is modified to: [1,2,3]
+//	
+//	Note:
+//	1 <= arr.length <= 10000
+//	0 <= arr[i] <= 9
+
+func duplicateZeros(arr []int)  {
+	l := len(arr)
+	for i := 0; i < l; i ++ {
+		if arr[i] == 0 {
+			for j := l-1; j > i; j -- {
+				arr[j] = arr[j-1]
+			}
+			i ++
+		}
+	}
+}
