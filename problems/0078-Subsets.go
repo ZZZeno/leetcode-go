8887c6744678db6cc06c18acf144f11ce7e73d47
Zeno <zenocbda@gmail.com>
2020-11-24 10:39:21 +0800

    Create 0078-Subsets.go

---
+++ problems/0078-Subsets.go
@@ -0,0 +1,28 @@
+package problems
+
+import "fmt"
+
+func subsets(nums []int) [][]int {
+	l := len(nums)
+	if l == 0 {
+		return [][]int{}
+	}
+	if l == 1 {
+		return [][]int{
+			{},
+			{nums[0]},
+		}
+	}
+	tmp := subsets(nums[1:])
+	res := subsets(nums[1:])
+	//fmt.Println(tmp)
+	for _, v := range tmp {
+		v = append(v, nums[0])
+		res = append(res, v)
+	}
+	return res
+}
+
+func Subsets(nums []int)  {
+	fmt.Printf("%v\n", subsets(nums))
+}
