03dc6cef6df7f14570bea043571909246b3e1748
Zeno <zenocbda@gmail.com>
2020-11-30 16:44:09 +0800

    Create KthSymbolInGrammar.go

---
+++ explore/Recursion/chapter5/KthSymbolInGrammar.go
@@ -0,0 +1,61 @@
+func kthGrammar(N int, K int) int {
+	if N == 1 {
+		return 0
+	}
+	if K == 1 {
+		return 0
+	}
+	if K == 2 {
+		return 1
+	}
+	leavesCnt := myPow(2, N-2)
+	if K > leavesCnt {
+		//right tree
+		leftRes := kthGrammar(N, K-leavesCnt)
+		if leftRes == 0 {
+			return 1
+		}
+		return 0
+
+	} else {
+		//left tree
+		parent := kthGrammar(N-1, (K+1)/2)
+        if parent == 0 {
+            if K % 2 == 0 {
+                return 1
+            }
+            return 0
+        }
+        if K % 2 == 0 {
+            return 0
+        }
+        return 1
+	}
+}
+
+func myPow(x int, n int) int {
+	if n == 0 {
+		return 1
+	}
+	temp := map[int]int{
+		1:  x,
+		-1: 1 / x,
+	}
+
+	return fromCache(x, n, temp)
+}
+
+func fromCache(x int, n int, cache map[int]int) int {
+	if cache[n] != 0 {
+		return cache[n]
+	}
+	a := fromCache(x, n/2, cache)
+	if n%2 == 0 {
+		return a * a
+	} else {
+		if n > 0 {
+			return a * a * x
+		}
+		return a * a * 1/x
+	}
+}
