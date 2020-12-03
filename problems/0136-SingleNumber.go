fc8ce4c270acf9c8604dbdd5f5e5f967ef315421
Zeno <zenocbda@gmail.com>
2020-12-03 12:29:21 +0800

    Update 0136-SingleNumber.go

--- problems/0136-SingleNumber.go
+++ problems/0136-SingleNumber.go
@@ -6,24 +6,14 @@ import (
 )
 
 func singleNumber(nums []int) int {
-	sort.Ints(nums)
-	if len(nums) == 1 {
-		return nums[0]
-	}
-	last := nums[0]
-	cnt := 1
-	for i := 1; i < len(nums); i ++ {
-		if nums[i] == last {
-			cnt += 1
-		} else {
-			if cnt == 1 {
-				return nums[i-1]
-			}
-			last = nums[i]
-			cnt = 1
-		}
-	}
-	return nums[len(nums)-1]
+    if len(nums) == 1 {
+        return nums[0]
+    }
+    last := nums[0]
+    for i := 1; i < len(nums); i ++ {
+        last = last ^ nums[i]
+    }
+    return last
 }
 
 func SingleNumber() {
