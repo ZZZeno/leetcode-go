0d46723f53f2539310801e507cc842e35c789d14
Zeno <zenocbda@gmail.com>
2021-01-07 15:57:11 +0800

    Create lock.go

---
+++ whatever/tips/lock.go
@@ -0,0 +1,30 @@
+package tips
+
+import "sync"
+
+type Storage interface {
+	Delete(key string)
+	Get(key string) string
+	Put(key, value string)
+}
+
+type StringStringMapWithLock map[string]string
+var lock = sync.RWMutex{}
+
+func (this StringStringMapWithLock) Delete(key string)  {
+	lock.Lock()
+	defer lock.Unlock()
+	delete(this, key)
+}
+
+func (this StringStringMapWithLock) Get(key string) string {
+	lock.Lock()
+	defer lock.Unlock()
+	return this[key]
+}
+
+func (this StringStringMapWithLock) Put(key, value string) {
+	lock.Lock()
+	defer lock.Unlock()
+	this[key] = value
+}
