9c6c14f311e5d932fccbb57777fbb73554438d7e
Zeno <zenocbda@gmail.com>
2021-07-21 14:22:43 +0800

    Update queue.go

--- utils/queue.go
+++ utils/queue.go
@@ -1,35 +1,17 @@
 package utils
 
-func IntArr2TreeNode(arr []int) *TreeNode {
-	if len(arr) == 0 {
+type Queue []*TreeNode
+
+func (q *Queue) Pop() *TreeNode {
+	if len(*q) == 0 {
 		return nil
 	}
-	queue := Queue{}
-	var head = &TreeNode{Val: arr[0]}
-	queue.Push(head)
-	var queueHead *TreeNode
-	for i := 1; i < len(arr); i++ {
-		for {
-			queueHead = queue.Pop()
-			if queueHead != nil {
-				break
-			}
-		}
-		queueHead.Left = newNode(arr[i])
-		queue.Push(queueHead.Left)
-		i += 1
-		if i >= len(arr) {
-			break
-		}
-		queueHead.Right = newNode(arr[i])
-		queue.Push(queueHead.Right)
-	}
-	return head
+	ret := (*q)[0]
+	*q = (*q)[1:len(*q)]
+	return ret
 }
 
-func newNode(i int) *TreeNode {
-	if i == 0 {
-		return nil
-	}
-	return &TreeNode{Val: i}
+func (q *Queue) Push(node *TreeNode) {
+	*q = append(*q, node)
 }
+
